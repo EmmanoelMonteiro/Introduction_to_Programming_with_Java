7ecb7706f93470eb4b3e938f00c89d96754c9445
Emmanoel Monteiro <emmanoeljr@gmail.com>
2025-06-28 14:52:03 -0300

    Inclusão de arquivo HelloWord

---
+++ src/HelloWorld.java
@@ -0,0 +1,17 @@
+//import java.util.Scanner;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Alô, mundo JAVA!");
+
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("Qual o seu nome: ");
+//        String nome = input.nextLine();  // Tipo não-primitivo
+//
+//        System.out.println("Alô "+ nome +", bem vindo ao mundo JAVA!");
+    }
+
+}
