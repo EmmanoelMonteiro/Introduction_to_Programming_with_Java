7c0177a69730aa22f4f6ce88aaf103dfb6016e8c
Emmanoel Monteiro <emmanoeljr@gmail.com>
2025-06-28 18:14:41 -0300

    Atualização de arquivos

---
+++ src/Pessoa.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Pessoa {
+    String nome;
+    String idade;
+
+    public Pessoa(String nome, String idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void pintNascimento(){
+        int anoNascimento = 2025 - Integer.parseInt(this.idade);
+        System.out.println(this.nome + " nasceu em " + anoNascimento);
+    }
+
+    public void printValidaIdade(){
+        boolean adulto = false;
+        if(Integer.parseInt(this.idade) >= 18){
+            adulto = true;
+        }
+        System.out.println("Adulto? " + adulto);
+    }
+}
